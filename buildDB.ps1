51147143b849ca61b210277d5d4524ba9810798c
unknown <dschenck@win.redhat.com>
2023-10-04 16:53:49 -0400

    Moved to a YAML-based deployment

--- buildDB.ps1
+++ buildDB.ps1
@@ -1,3 +1,7 @@
 $a = (kubectl get pods | select-string '^quote([^\s]+)-(?!deploy)') -match 'quote([^\s]+)'; $podname = $matches[0]
-oc cp ./quotes.json ${podname}:quotes.json
-oc exec ${podname} -- mongoimport -u admin -p quote -c quote -d quote --authenticationDatabase admin quotes.json
+
+oc exec ${podname} -- mongosh -u admin -p quote --authenticationDatabase admin --eval 'use quotes' --eval 'db.createUser({user: "quotes", pwd: "quotes", roles: [{ role: "dbAdmin", db: "quotes" },{role: "readWrite", db: "quotes"}]})' --quiet
+
+
+oc cp ./quotes.json ${podname}:/tmp/quotes.json
+oc exec ${podname} -- mongoimport -u admin -p quote -c quote -d quote --authenticationDatabase admin /tmp/quotes.json
--- buildDB.sh
+++ buildDB.sh
@@ -2,5 +2,5 @@
 export PODNAME=$(oc get pods -o custom-columns=POD:.metadata.name --no-headers | grep -v 'deploy$' | grep quote)
 echo $PODNAME
 
-oc cp ./quotes.json $PODNAME:quotes.json
-oc exec $PODNAME -- mongoimport -u admin -p quote -c quote -d quote --authenticationDatabase admin quotes.json
+oc cp ./quotes.json $PODNAME:/tmp/quotes.json
+oc exec $PODNAME -- mongoimport -u admin -p quote -c quote -d quote --authenticationDatabase admin /tmp/quotes.json
