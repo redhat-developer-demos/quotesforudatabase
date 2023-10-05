92dcc6f9de9df1bd9c5f1425cf6250b985b1e68f
unknown <dschenck@win.redhat.com>
2023-10-05 11:46:34 -0400

    Improvements

--- buildDB.ps1
+++ buildDB.ps1
@@ -1,7 +1,16 @@
+echo 'BUILD AND POPULATE QUOTE DATABASE'
+echo '---------------------------------'
+
+echo 'getting pod name ...'
 $a = (kubectl get pods | select-string '^quote([^\s]+)-(?!deploy)') -match 'quote([^\s]+)'; $podname = $matches[0]
 
-oc exec ${podname} -- mongosh -u admin -p quote --authenticationDatabase admin --eval 'use quotes' --eval 'db.createUser({user: "quotes", pwd: "quotes", roles: [{ role: "dbAdmin", db: "quotes" },{role: "readWrite", db: "quotes"}]})' --quiet
+echo 'creating database and user account...'
+oc exec ${podname} -- mongosh -u admin -p quote --authenticationDatabase admin --eval 'use quote' --eval 'db.createUser({user: "quote", pwd: "quote", roles: [{ role: "dbAdmin", db: "quote" },{role: "readWrite", db: "quote"}]})' --quiet
 
-
+echo 'copying quotes to temporary folder in pod...'
 oc cp ./quotes.json ${podname}:/tmp/quotes.json
+
+echo 'importing quotes data...'
 oc exec ${podname} -- mongoimport -u admin -p quote -c quote -d quote --authenticationDatabase admin /tmp/quotes.json
+
+echo 'FINISHED'
--- buildDB.sh
+++ buildDB.sh
@@ -1,6 +1,17 @@
+echo 'BUILD AND POPULATE QUOTE DATABASE'
+echo '---------------------------------'
 
+echo 'getting pod name ...'
 export PODNAME=$(oc get pods -o custom-columns=POD:.metadata.name --no-headers | grep -v 'deploy$' | grep quote)
 echo $PODNAME
 
+echo 'creating database and user account...'
+oc exec $PODNAME -- mongosh -u admin -p quote --authenticationDatabase admin --eval 'use quote' --eval 'db.createUser({user: "quote", pwd: "quote", roles: [{ role: "dbAdmin", db: "quote" },{role: "readWrite", db: "quote"}]})' --quiet
+
+echo 'copying quotes to temporary folder in pod...'
 oc cp ./quotes.json $PODNAME:/tmp/quotes.json
+
+echo 'importing quotes data...'
 oc exec $PODNAME -- mongoimport -u admin -p quote -c quote -d quote --authenticationDatabase admin /tmp/quotes.json
+
+echo 'FINISHED'
